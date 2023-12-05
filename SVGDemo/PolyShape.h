a2b1785119eaa1c69aa423e51414623bd2492899
dihungg <149929548+dihungg@users.noreply.github.com>
2023-12-05 15:16:57 +0700

    Update PolyShape.h
    Signed-off-by: dihungg <149929548+dihungg@users.noreply.github.com>

--- SVGDemo/PolyShape.h
+++ SVGDemo/PolyShape.h
@@ -7,7 +7,7 @@
 #include <gdiplus.h>
 #include "Point.h"
 #include "Shape.h"
-
+#include "Transform.h"
 
 using namespace std;
 using namespace rapidxml;
@@ -37,7 +37,7 @@ public:
 	void SetPolyLine(int* rgb, int thickness, vector<Point2D> points, int* fill_rgb, 
 					 double fill_opacity, double stroke_opacity);
 	void fillPoline(HDC);
-	VOID OnPaint(HDC hdc);
+	VOID OnPaint(HDC hdc, vector<Transform>& transform);
 };
 
 
@@ -46,5 +46,5 @@ class PolyGon : public PolyShape {
 public:
 	void SetPolyGon(int* rgb, int* fill_rgb, int thickness, vector<Point2D> points, double fill_opacity, double stroke_opacity);
 	void fillPolygon(HDC);
-	VOID OnPaint(HDC hdc);
+	VOID OnPaint(HDC hdc, vector<Transform>& transform);
 };
