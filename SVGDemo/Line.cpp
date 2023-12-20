a3f43811c2a12f13a34d3468a21f62a2ee8e5e96
thienD <119030626+duccodown@users.noreply.github.com>
2023-12-20 17:28:41 +0700

    Update Line.cpp gradient
    Signed-off-by: thienD <119030626+duccodown@users.noreply.github.com>

--- SVGDemo/Line.cpp
+++ SVGDemo/Line.cpp
@@ -16,6 +16,41 @@ void Line::SetLine(int* rgb, Point2D start, Point2D end, int thickness, double s
     this->stroke_opacity = stroke_opacity;
 }
 
+void Line::myLinearGradientBrush(double* firstrgb, double* secondrgb)
+{
+    Graphics graphics(hdc);
+    LinearGradientBrush linearBrush(
+        Point(50, 10), //associated with the first color
+        Point(150, 10), //associated with the second color
+        Color(255 * firstrgb[0], firstrgb[1], firstrgb[2], firstrgb[3]),
+        Color(255 * secondrgb[0], secondrgb[1], secondrgb[2], secondrgb[3]));
+
+    linearBrush.SetGammaCorrection(TRUE);
+    Pen pen(&linearBrush);
+    graphics.DrawLine(&pen, start.GetX(), start.GetY(), end.GetX(), end.GetY());
+}
+
+void Line::gradientBrushPath(double* firstrgb, double* secondrgb)
+{
+    // Create a path that consists of a single ellipse.
+    Graphics graphics(this->hdc);
+    GraphicsPath path;
+    path.AddLine(start.GetX(), start.GetY(), end.GetX(), end.GetY());
+
+    // Use the path to construct a brush.
+    PathGradientBrush pthGrBrush(&path);
+    pthGrBrush.SetGammaCorrection(TRUE);
+
+    // Set the color at the center of the path to blue.
+    pthGrBrush.SetCenterColor(Color(255 * firstrgb[0], firstrgb[1], firstrgb[2], firstrgb[3]));
+
+    // Set the color along the entire boundary of the path to aqua.
+    Color colors[] = { Color(255 * secondrgb[0], secondrgb[1], secondrgb[2], secondrgb[3]) };
+    int count = 1;
+    pthGrBrush.SetSurroundColors(colors, &count);
+    Pen pen(&pthGrBrush);
+    graphics.DrawLine(&pen, start.GetX(), start.GetY(), end.GetX(), end.GetY());
+}
 
 VOID Line::OnPaint(HDC hdc, double opacity) {
     Graphics graphics(hdc);
