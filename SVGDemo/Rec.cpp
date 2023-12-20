838e60669d487f7b9cc3370839f7844455111dde
thienD <119030626+duccodown@users.noreply.github.com>
2023-12-20 17:34:48 +0700

    Update Rec.cpp gradient
    Signed-off-by: thienD <119030626+duccodown@users.noreply.github.com>

--- SVGDemo/Rec.cpp
+++ SVGDemo/Rec.cpp
@@ -56,14 +56,48 @@ void _Rectangle::fillRect(HDC hdc, double opacity)
     graphics.FillRectangle(&solidBrush, start.GetX(), start.GetY(), width, height);
 }
 
+void _Rectangle::myLinearGradientBrush(double* firstrgb, double* secondrgb)//these arr need 4 elements (alpha, reb, green, blue)
+{
+    Graphics graphics(hdc);
+    LinearGradientBrush linearBrush(
+        Point(50, 10), //associated with the first color
+        Point(150, 10), //associated with the second color
+        Color(255 * firstrgb[0], firstrgb[1], firstrgb[2], firstrgb[3]),
+        Color(255 * secondrgb[0], secondrgb[1], secondrgb[2], secondrgb[3]));
+
+    linearBrush.SetGammaCorrection(TRUE);
+    graphics.FillRectangle(&linearBrush, start.GetX(), start.GetY(), width, height);
+}
+
+void _Rectangle::gradientBrushPath(double* firstrgb, double* secondrgb)
+{
+    // Create a path that consists of a single ellipse.
+    Graphics graphics(this->hdc);
+    GraphicsPath path;
+    RectF r(start.GetX(), start.GetY(), width, height);
+    path.AddRectangle(r);
+
+    // Use the path to construct a brush.
+    PathGradientBrush pthGrBrush(&path);
+    pthGrBrush.SetGammaCorrection(TRUE);
+
+    // Set the color at the center of the path to blue.
+    pthGrBrush.SetCenterColor(Color(255 * firstrgb[0], firstrgb[1], firstrgb[2], firstrgb[3]));
+
+    // Set the color along the entire boundary of the path to aqua.
+    Color colors[] = { Color(255 * secondrgb[0], secondrgb[1], secondrgb[2], secondrgb[3]) };
+    int count = 1;
+    pthGrBrush.SetSurroundColors(colors, &count);
+
+    graphics.FillRectangle(&pthGrBrush, start.GetX(), start.GetY(), width, height);
+}
+
 VOID _Rectangle::OnPaint(HDC hdc, double stroke_opacity) {
     Graphics graphics(hdc);
     int alpha = 255 * stroke_opacity;
     Pen      pen(Color(alpha, this->rgb[0], this->rgb[1], this->rgb[2]), this->thickness);
     graphics.DrawRectangle(&pen, start.GetX(), start.GetY(), width, height);
 }
-
-
 
 //void RoundRectangle::SetRoundRec(HDC hdc, int* rgb, Point2D start, int height, int width, int thickness) {
 //    this->hdc = hdc;
